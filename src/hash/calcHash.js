5abf47910866610bc975a5cf897515df6a12f087
Oksana Fedotova <oxana-fedotova@yandex.ru>
2023-06-17 19:08:30 +0300

    fix: revert "refactor: add functon to get path"
    This reverts commit 7eef6de7f482eb5dbe6bcf9bf85bbc93de41b988.

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,11 +1,5 @@
-import fs from 'fs';
-import { createHash } from 'node:crypto'
-import { getPath } from '../utils.js';
-
 const calculateHash = async () => {
-  const pathFile = getPath(import.meta.url, 'files/fileToCalculateHashFor.txt');
-  const content = await fs.promises.readFile(pathFile);
-  console.log(createHash('sha256').update(content).digest('hex'));
+    // Write your code here 
 };
 
 await calculateHash();
