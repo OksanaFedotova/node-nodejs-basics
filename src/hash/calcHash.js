7eef6de7f482eb5dbe6bcf9bf85bbc93de41b988
Oksana Fedotova <oxana-fedotova@yandex.ru>
2023-06-17 19:05:43 +0300

    refactor: add function to get path

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,11 @@
+import fs from 'fs';
+import { createHash } from 'node:crypto'
+import { getPath } from '../utils.js';
+
 const calculateHash = async () => {
-    // Write your code here 
+  const pathFile = getPath(import.meta.url, 'files/fileToCalculateHashFor.txt');
+  const content = await fs.promises.readFile(pathFile);
+  console.log(createHash('sha256').update(content).digest('hex'));
 };
 
 await calculateHash();
