17f6ecd1a09fe807a7cec3166facc5e231837c0f
Oksana Fedotova <oxana-fedotova@yandex.ru>
2023-06-18 17:05:55 +0300

    feat: zip completed

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,13 @@
+import fs from 'fs';
+import { createGzip } from 'zlib';
+import { getPath } from '../utils.js'
+
 const compress = async () => {
-    // Write your code here 
+  const pathName = getPath(import.meta.url, 'files/fileToCompress.txt');
+  const readStream = fs.createReadStream(pathName);
+  readStream
+    .pipe(createGzip())
+    .pipe(fs.createWriteStream(getPath(import.meta.url, 'files/archive.gz')))
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,13 @@
+import fs from 'fs';
+import { createUnzip } from 'zlib';
+import { getPath } from '../utils.js'
+
 const decompress = async () => {
-    // Write your code here 
+  const pathName = getPath(import.meta.url, 'files/archive.gz');
+  const readStream = fs.createReadStream(pathName);
+  readStream
+    .pipe(createUnzip())
+    .pipe(fs.createWriteStream(getPath(import.meta.url, 'files/fileToCompress.txt')))
 };
 
 await decompress();
