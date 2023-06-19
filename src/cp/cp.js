429e702f951514ce8f14ff30013c281aeee0caf7
Oksana Fedotova <oxana-fedotova@yandex.ru>
2023-06-19 13:26:59 +0300

    feat: cp completed

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,12 @@
+import { fork } from 'node:child_process';
+import { getPath } from '../utils.js';
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  const pathName = getPath(import.meta.url, 'files/script.js');
+  const child = fork(pathName, args, {stdio: 'pipe'});
+  process.stdin.pipe(child.stdin);
+  child.stdout.pipe(process.stdout);
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(['-ls']);
