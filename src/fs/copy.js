4793e5479899df8dcd7a03f25218f811f64479d6
Oksana Fedotova <oxana-fedotova@yandex.ru>
2023-06-19 14:10:38 +0300

    fix: revert add await for fs.promises.readdir"
    This reverts commit 23f98391bd0ab450dc75ed471e908f7073c7382b.

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -9,8 +9,8 @@ const copy = async () => {
 
   const copyFiles = async () => {
     try {
-      const files = await fs.promises.readdir(pathFiles, {withFileTypes: true});
-      if (files.length) await fs.promises.mkdir(pathCopy);
+      await fs.promises.mkdir(pathCopy);
+      const files = fs.promises.readdir(pathFiles, {withFileTypes: true});
       for (const file of files) {
         const filePath = path.join(__dirname, 'files', file.name);
         const copyPath = path.join(__dirname, 'files_copy', file.name);
